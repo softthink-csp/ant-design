8c6edbffa79a4625ce7f011b8b8b6a3c76a3b7f4
teishouhei <bing527309700@163.com>
2020-07-10 09:25:34 +0800

    add ant-desgin-vue plugin

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+import {button} from 'ant-design-vue'
 import router from "./router";
 import store from "./store";
+//import 'ant-design-vue/dist/antd.less'
+// import 'ant-design-vue/lib/button/style'
+// import button from 'ant-design-vue/lib/button'
 
 Vue.config.productionTip = false;
+Vue.use(button)
 
 new Vue({
   router,
